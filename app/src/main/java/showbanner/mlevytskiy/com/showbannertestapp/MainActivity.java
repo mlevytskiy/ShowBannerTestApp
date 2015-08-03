72a43fcb3cf4e2e81505a675048b4276b7e4012c
mlevytskiy <m.levytskiy@gmail.com>
2015-08-03 13:36:10 +0300

    show banner, when my app not in foreground

--- app/src/main/java/showbanner/mlevytskiy/com/showbannertestapp/MainActivity.java
+++ app/src/main/java/showbanner/mlevytskiy/com/showbannertestapp/MainActivity.java
@@ -2,12 +2,11 @@ package showbanner.mlevytskiy.com.showbannertestapp;
 
 import android.content.*;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import showbanner.mlevytskiy.com.showbannertestapp.core.AnyActivity;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AnyActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,27 +15,5 @@ public class MainActivity extends ActionBarActivity {
 
         Intent startIntent = new Intent(getApplicationContext(), ImplSFService.class);
         startService(startIntent);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
